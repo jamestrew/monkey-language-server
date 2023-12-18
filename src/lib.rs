b7dd5620c11346eeab7a8bb0bda30d5b97e2bd30
James Trew <j.trew10@gmail.com>
2023-12-17 22:15:02 -0500

    add logging

--- src/lib.rs
+++ src/lib.rs
@@ -8,5 +8,55 @@ mod types;
 #[cfg(test)]
 mod test_util;
 
+use std::collections::HashMap;
+use std::sync::Arc;
+
 pub use ast::Node;
+pub use eval::Eval;
+use eval::{Env as EvalEnv, Object};
 pub use parser::Parser;
+use tower_lsp::lsp_types::Diagnostic;
+use types::Spanned;
+
+pub struct Env {
+    store: HashMap<String, Arc<Spanned<Object>>>,
+    refs: Vec<Arc<Spanned<String>>>,
+    children: Vec<Env>,
+}
+
+impl From<EvalEnv> for Env {
+    fn from(env: EvalEnv) -> Self {
+        let env = env.take_environment();
+        Self {
+            store: env.store,
+            refs: env.refs,
+            children: env
+                .children
+                .into_iter()
+                .map(|child| child.into())
+                .collect::<Vec<_>>(),
+        }
+    }
+}
+
+pub fn analyze_source(source: &str) -> (Vec<Diagnostic>, Env) {
+    let program = Parser::from_source(source).parse_program();
+    let (eval_env, diags) = Eval::eval_program(program.nodes);
+
+    let diags = diags
+        .iter()
+        .map(|diag| {
+            Diagnostic::new(
+                diag.lsp_range(),
+                Some(diag.severity()),
+                None,
+                None,
+                diag.to_string(),
+                None,
+                None,
+            )
+        })
+        .collect::<Vec<_>>();
+
+    (diags, eval_env.into())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,30 @@
+use std::sync::Arc;
+
 use monkey_language_server::*;
+use tokio::sync::Mutex;
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+use tracing::info;
 
-#[derive(Debug)]
 struct Backend {
     client: Client,
+    env: Arc<Mutex<Option<Env>>>,
 }
 
 impl Backend {
+    pub fn new(client: Client) -> Self {
+        Self {
+            client,
+            env: Default::default(),
+        }
+    }
+
     async fn on_change(&self, uri: Url, text: String, version: i32) {
-        let program = Parser::from_source(&text).parse_program();
-        let diagnostics = program
-            .nodes
-            .into_iter()
-            .filter_map(|node| match node {
-                Node::Statement(_) => None,
-                Node::Error(err) => Some(Diagnostic::new_simple(err.lsp_range(), err.to_string())),
-            })
-            .collect::<_>();
+        info!("on_change");
+        let (diagnostics, new_env) = analyze_source(&text);
+        let mut old_env = self.env.lock().await;
+        *old_env = Some(new_env);
 
         self.client
             .publish_diagnostics(uri.clone(), diagnostics, Some(version))
@@ -69,6 +75,21 @@ impl LanguageServer for Backend {
         .await
     }
 
+    async fn goto_definition(
+        &self,
+        params: GotoDefinitionParams,
+    ) -> Result<Option<GotoDefinitionResponse>> {
+        let _ = params;
+
+        // get cursor position
+        let _pos = params.text_document_position_params.position;
+        // get eval'ed env (should be in our backend struct)
+        // check env refs for under cursor ident name
+        // find correct ident in our env store
+
+        todo!()
+    }
+
     async fn shutdown(&self) -> Result<()> {
         Ok(())
     }
@@ -90,9 +111,15 @@ impl LanguageServer for Backend {
 
 #[tokio::main]
 async fn main() {
+    let file_appender = tracing_appender::rolling::daily("/tmp", "monkeylsp.log");
+    let (non_blocking, _guard) = tracing_appender::non_blocking(file_appender);
+
+    tracing_subscriber::fmt().with_writer(non_blocking).init();
+
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
-    let (service, socket) = LspService::new(|client| Backend { client });
+    info!("starting server");
+    let (service, socket) = LspService::new(Backend::new);
     Server::new(stdin, stdout, socket).serve(service).await;
 }
