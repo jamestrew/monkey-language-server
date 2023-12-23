35ff7eee752138dddc7909cc810335a668298b51
James Trew <j.trew10@gmail.com>
2023-12-23 17:27:17 -0500

    add range to function expression

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -399,7 +399,7 @@ impl<'source, TP: TokenProvider<'source>> Parser<'source, TP> {
         if body.is_err() {
             self.sync()?;
         }
-        Ok(Function::new(fn_token, params, body).into())
+        Ok(Function::new(fn_token, params, body, self.prev_span.end).into())
     }
 
     fn parse_fn_params(&mut self) -> Result<Vec<ExprResult<'source>>, SpannedError> {
